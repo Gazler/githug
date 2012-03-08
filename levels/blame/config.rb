10b0a1429726fc976208e506a1ced381cbd25841
Gary Rennie <webmaster@gazler.com>
2012-03-08 23:04:02 +0000

    added blame level

---
+++ levels/blame/config.rb
@@ -0,0 +1,16 @@
+class Config
+  attr_accessor :name, :password
+  def initialize(name, password = nil, options = {})
+    @name = name
+    @password = password || "i<3evil"
+    
+    if options[:downcase]
+      @name.downcase!
+    end
+
+    if options[:upcase]
+      @name.upcase!
+    end
+
+  end
+end
