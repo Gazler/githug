c1a21d910fd09bde144540745fb727c47f851cd8
Gary Rennie <webmaster@gazler.com>
2012-03-12 19:09:36 +0000

    added clone_to_folder level

---
+++ levels/clone_to_folder.rb
@@ -0,0 +1,10 @@
+difficulty 1
+description "Clone the repository at https://github.com/Gazler/cloneme to 'my_cloned_repo'"
+
+solution do
+  repo("my_cloned_repo").commit("157b2b61f29ab9df45f31c7cd9cb5d8ff06ecde4")
+end
+
+hint do
+  puts "You should have a look at this site: https://github.com/Gazler/cloneme"
+end
