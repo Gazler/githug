4be0d25235c9af758f6bd4b2d4f623fee373fba4
Antoine Qu'hen <antoinequhen@gmail.com>
2012-03-20 17:32:09 -0400

    Add a level `rename commit`

---
+++ levels/rename_commit.rb
@@ -0,0 +1,22 @@
+difficulty 3
+description "Correct the typos in the message of your last commit."
+
+setup do
+  repo.init
+
+  FileUtils.touch "README"
+  repo.add        "README"
+  repo.commit_all "Adding README"
+
+  FileUtils.touch "file1"
+  repo.add        "file1"
+  repo.commit_all "A fresh commmit"
+end
+
+solution do
+  repo.commits.first.message == "A fresh commit"
+end
+
+hint do
+  puts "Take a look the -i flag of the rebase command"
+end
