26fa2cd4eb21f4f07548b3edb7ba50fe047bfa0e
John Crepezzi <john.crepezzi@gmail.com>
2012-03-22 16:17:25 -0400

    Fix some formatting

--- levels/merge.rb
+++ levels/merge.rb
@@ -2,13 +2,13 @@ difficulty 2
 description "We have a file in the branch 'feature'; Let's merge it to the master branch"
 
 setup do
-      init_from_level
+  init_from_level
 end
 
 solution do
-      `ls`.strip().split() == ["file1", "file2"]
+  `ls`.strip().split() == ["file1", "file2"]
 end
 
 hint do
-      puts "You want to research the `git merge` command"
+  puts "You want to research the `git merge` command"
 end
