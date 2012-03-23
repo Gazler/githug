3d66453cd5e0297b8e29a541330aad88031280fc
Gary Rennie <webmaster@gazler.com>
2012-03-23 19:48:53 +0000

    all 0.1.7 levels working in windows

--- levels/merge.rb
+++ levels/merge.rb
@@ -2,11 +2,11 @@ difficulty 2
 description "We have a file in the branch 'feature'; Let's merge it to the master branch"
 
 setup do
-      init_from_level
+	init_from_level
 end
 
 solution do
-      `ls`.strip().split() == ["file1", "file2"]
+	File.exists?("file1")	&& File.exists?("file2") 
 end
 
 hint do
