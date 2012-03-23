fbccc2e4e4531506bb3a0c044c273c0a9f0cd378
Gary Rennie <webmaster@gazler.com>
2012-03-23 00:31:09 +0000

    Working in windows up until the merge level

--- levels/branch_at.rb
+++ levels/branch_at.rb
@@ -15,9 +15,9 @@ setup do
 end
 
 solution do
-  solved = false
-  solved = true if repo.branches.map(&:name).include?("test_branch") and (repo.commits("test_branch").last.message ==  "Updating file1")
-  solved
+  return false unless repo.branches.map(&:name).include?("test_branch")
+  repo.commits("test_branch").each { |commit| return false if commit.message ==  "Updating file1 again" }
+  true
 end
 
 hint do
