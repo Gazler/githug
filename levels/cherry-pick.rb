58984556c62db45a854f57b1bec0645b1a56ce06
Andrey <aslushnikov@gmail.com>
2012-03-28 03:05:49 +0400

    Added cherry-pick level

---
+++ levels/cherry-pick.rb
@@ -0,0 +1,16 @@
+difficulty 3
+description "Your new feature doesn't worth its time and you're going to delete it. But it has one commit that fills in README file, and you want this commit to be on the master as well"
+
+setup do
+    init_from_level
+end
+
+solution do
+  return false unless repo.commits[1].message == "Added fancy branded output"
+  return false unless repo.commits[0].message == "Filled in README.md with proper input"
+  true
+end
+
+hint do
+  puts "Sneak peak at the `cherry-pick` command"
+end
