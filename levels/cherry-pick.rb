3cbe971aa9e1703c1b4157c962bff42a16892ca0
Albin Theander <Albin.Theander@jayway.com>
2012-04-03 15:52:00 +0200

    Fixed solution code for cherry-pick level

--- levels/cherry-pick.rb
+++ levels/cherry-pick.rb
@@ -1,14 +1,15 @@
 difficulty 3
-description "Your new feature doesn't worth its time and you're going to delete it. But it has one commit that fills in README file, and you want this commit to be on the master as well"
+description "Your new feature isn't worth the time and you're going to delete it. But it has one commit that fills in README file, and you want this commit to be on the master as well"
 
 setup do
     init_from_level
 end
 
 solution do
-  return false unless repo.commits[1].message == "Added fancy branded output"
-  return false unless repo.commits[0].message == "Filled in README.md with proper input"
-  true
+  solved = true
+  solved = false unless repo.commits[1].message == "Added fancy branded output"
+  solved = false unless repo.commits[0].message == "Filled in README.md with proper input"
+  solved
 end
 
 hint do
