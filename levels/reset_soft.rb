053c9ac0dc9038ce14ce9d4148749b953176cc05
Thameera Senanayaka <thameera123@gmail.com>
2012-10-21 21:04:42 +0530

    Added reset soft level
    Signed-off-by: Thameera Senanayaka <thameera123@gmail.com>

---
+++ levels/reset_soft.rb
@@ -0,0 +1,22 @@
+difficulty 2
+description "You committed too soon. Now you want to undo the last commit, while keeping the index."
+
+setup do
+  repo.init
+  FileUtils.touch("README")
+  repo.add("README")
+  repo.commit_all("Initial commit")
+  FileUtils.touch("newfile.rb")
+  repo.add("newfile.rb")
+  repo.commit_all("Premature commit")
+end
+
+solution do
+  return false unless File.exists?("newfile.rb") && repo.status.files.keys.include?("newfile.rb")
+  return false if repo.status.files["newfile.rb"].untracked
+  true
+end
+
+hint do
+  puts "What are some options you can use with 'git reset'?"
+end
