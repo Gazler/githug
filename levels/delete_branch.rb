a463db7d2bdf53fa0cbc5f6018fcca57e9aa0714
Daniel Smilansky <dsmilansky@gmail.com>
2013-12-03 23:25:19 -0800

    delete branch folder

--- levels/delete_branch.rb
+++ levels/delete_branch.rb
@@ -3,12 +3,7 @@ difficulty 2
 description "You have created too many branches for your project. There is an old branch in your repo called 'delete_me', you should delete it."
 
 setup do
-  repo.init
-  FileUtils.touch("README")
-  repo.add "README"
-  repo.commit_all("Initial commit")
-  repo.git.native :checkout, {"b" => true}, 'delete_me'
-  repo.git.native :checkout, {}, 'master'
+  init_from_level
 end
 
 solution do
