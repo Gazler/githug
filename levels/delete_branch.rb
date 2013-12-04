48f406b9385523e0b060182baa9f84a751287612
Daniel Smilansky <dsmilansky@gmail.com>
2013-12-04 00:05:19 -0800

    delete branch level final

--- levels/delete_branch.rb
+++ levels/delete_branch.rb
@@ -3,7 +3,7 @@ difficulty 2
 description "You have created too many branches for your project. There is an old branch in your repo called 'delete_me', you should delete it."
 
 setup do
-  init_from_level
+    init_from_level
 end
 
 solution do
