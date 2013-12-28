6dcbfa056109404deb59aafd4d14fbf414c7bbda
Victor Medina <medina5000@gmail.com>
2013-12-28 02:59:38 -0800

    simplify tasks, ruby 1.8.7 is failing

--- levels/restructure.rb
+++ levels/restructure.rb
@@ -8,21 +8,10 @@ setup do
   FileUtils.touch("index.html")
   repo.add("index.html")
   repo.commit_all("adding index page.")
-
-  FileUtils.touch("about.html")
-  repo.add("about.html")
-  repo.commit_all("adding about page.")
-
-  FileUtils.touch("contact.html")
-  repo.add("contact.html")
-  repo.commit_all("adding contact page.")
 end
 
 solution do
-  index = repo.status["index.html"].type == "D" && repo.status["index.html"].stage.nil? && repo.status["src/index.html"].type == "A"
-  about = repo.status["about.html"].type == "D" && repo.status["about.html"].stage.nil? && repo.status["src/about.html"].type == "A"
-  contact = repo.status["contact.html"].type == "D" && repo.status["contact.html"].stage.nil? && repo.status["src/contact.html"].type == "A"
-  index && about && contact
+  repo.status["index.html"].type == "D" && repo.status["index.html"].stage.nil? && repo.status["src/index.html"].type == "A"  
 end
 
 hint do
