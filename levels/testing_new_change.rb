6b18c9f0d597ca554be37a64730c1cc88be527e9
Daniel Dec <ddec@future-processing.com>
2016-05-17 00:53:02 +0200

    Polskie znaki

--- levels/testing_new_change.rb
+++ levels/testing_new_change.rb
@@ -1,6 +1,6 @@
 difficulty 2
-description "Kiedyś ta prezentacja wyglądała nienajgorzej. Jednak zaczęło nad nią pracować zbyt dużo osób o bardzo
-późnych porach i ktoś wprowadził zmianę która uniemożliwia jej odczyt. Znajdź winnego, hash commita oraz nr lini w której powstał błąd"
+description "Kiedys ta prezentacja wygladala nienajgorzej. Jednak zaczelo nad nia pracowac zbyt duzo osob o bardzo
+poznych porach i ktos wprowadzil zmiane ktora uniemozliwia jej odczyt. Znajdz winnego, hash commita oraz nr lini w ktorej powstal blad"
 
 
 setup do
@@ -9,11 +9,11 @@ end
 
 solution do
   offender = repo.commit("dd797d1a0edbcbae317940eb289528763cd53298").author.name
-  request("W którym commicie prezentacja po uruchomieniu jest nieczytelna?") == "dd797d1a0edbcbae317940eb289528763cd53298"
-  request("Kto wprowadził tą zmianę").downcase == offender.downcase
-  request("Która linia spowodowała błąd") == 208
+  request("W ktorym commicie prezentacja po uruchomieniu jest nieczytelna?") == "dd797d1a0edbcbae317940eb289528763cd53298"
+  request("Kto wprowadzil ta zmiane?").downcase == offender.downcase
+  request("Ktora linia spowodowala blad?") == 208
 end
 
 hint do
-  puts "git blame, git checkout, git diff - powinny pomóc"
+  puts "git blame, git checkout, git diff - powinny pomoc"
 end
