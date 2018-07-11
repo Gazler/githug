baf4eae6d36dfe0c31952e6dd9920c8dd713a300
Tony Khamis <tkhamis@redhat.com>
2018-07-11 18:09:14 +0300

    Imporoved the hint of the  test grep

--- levels/number_of_files_committed.rb
+++ levels/number_of_files_committed.rb
@@ -47,5 +47,5 @@ solution do
 end
 
 hint do
-  puts "You are looking for a command to identify the status of the repository."
+  puts "You are looking for a command to identify the status of the repository, resembles a linux command."
 end
