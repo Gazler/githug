f85593ac9c79bbd3361bd8d7081425582177e82d
GhostPandaGP <aleks98_27@mail.ru>
2020-09-13 21:15:42 +0300

    Added variant for new level

---
+++ levels/aliases.rb
@@ -0,0 +1,15 @@
+difficulty 2
+description "You need to make several aliases."
+
+setup do
+  repo.init
+end
+
+solution do
+  # TODO make code for checking aliases of user
+end
+
+hint do
+  puts ["Read this article: https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases"]
+end
+
