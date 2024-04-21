33fcbc581b18df58c63b63ab82f1e5b8b547df8f
Norwid Behrnd <nbehrnd@yahoo.com>
2024-04-21 18:47:45 +0200

    fix(submodule.rb): explicitly run branch master
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- levels/submodule.rb
+++ levels/submodule.rb
@@ -2,19 +2,20 @@ difficulty 2
 description "You want to include the files from the following repo: `https://github.com/jackmaney/githug-include-me` into the folder `./githug-include-me`. Do this without manually cloning the repo or copying the files from the repo into this repo."
 
 setup do
-    repo.init
+  repo.init
+  system "git branch -m master"
 end
 
 solution do
-    return false if not File.directory?("./githug-include-me")
-    return false if not File.exist?("./githug-include-me/README.md")
-    return false if not File.exist?("./githug-include-me/.git")
-    return false if File.directory?("./githug-include-me/.git")
-    return false if not File.exist?(".gitmodules")
+  return false if not File.directory?("./githug-include-me")
+  return false if not File.exist?("./githug-include-me/README.md")
+  return false if not File.exist?("./githug-include-me/.git")
+  return false if File.directory?("./githug-include-me/.git")
+  return false if not File.exist?(".gitmodules")
 
-    return true
+  return true
 end
 
 hint do
-    puts "Take a look at `git submodule`."
+  puts "Take a look at `git submodule`."
 end
