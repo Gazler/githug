13dfbf7000679d0f97df51b542fc17f3c47ee760
Norwid Behrnd <nbehrnd@yahoo.com>
2024-04-21 18:30:16 +0200

    fix(merge.rb): explicitly run branch master
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- levels/merge.rb
+++ levels/merge.rb
@@ -1,12 +1,13 @@
 difficulty 2
-description "We have a file in the branch 'feature'; Let's merge it to the master branch."
+description "We have a file in the branch 'feature'. Let's merge it with the master branch."
 
 setup do
-	init_from_level
+  init_from_level
+  system "git branch -m master"
 end
 
 solution do
-	File.exists?("file1")	&& File.exists?("file2")
+  File.exists?("file1")	&& File.exists?("file2")
 end
 
 hint do
